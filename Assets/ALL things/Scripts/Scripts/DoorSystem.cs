28fafbd717b5220ed0ed3db3673d358362c3e3d7
hsugoya <hsugoya@gmail.com>
2023-02-05 10:03:52 +0800

    整合

--- Assets/ALL things/Scripts/Scripts/DoorSystem.cs
+++ Assets/ALL things/Scripts/Scripts/DoorSystem.cs
@@ -7,12 +7,20 @@ public class DoorSystem : MonoBehaviour
     public List<string> Keylist;
     [SerializeField] private DoorAnimation door;
     [SerializeField] private IDdooranimation IDdoor;
+    private bool buttontrigger = false;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && buttontrigger)
+        {
+            Destroy(GameObject.FindWithTag("Button1"));
+            door.opendoor();
+        }
+    }
     void OnTriggerEnter2D(Collider2D collide)
     {
         if (collide.gameObject.CompareTag("Button1"))
         {
-            Destroy(GameObject.FindWithTag("Button1"));
-            door.opendoor();
+            buttontrigger= true;
         }
         if (collide.gameObject.CompareTag("IDRegion"))
         {
